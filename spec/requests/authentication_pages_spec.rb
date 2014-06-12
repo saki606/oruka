bc8e9f67b420cd0d0813650a135c841a4392a59a
Go Fujita <saki606@gmail.com>
2014-06-12 23:40:02 +0900

    Finished sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,19 @@ describe "Authentication" do
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end
+  
+  before { visit signin_path }
+
+     describe "with invalid information" do
+       before { click_button "Sign in" }
+
+       it { should have_title('Sign in') }
+       it { should have_selector('div.alert.alert-error') }
+
+       describe "after visiting another page" do
+         before { click_link "Home" }
+         it { should_not have_selector('div.alert.alert-error') }
+       end
+     end
+   end
 end
